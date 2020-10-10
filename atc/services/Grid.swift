74e791aba051ebfb75cd4341427ed3a0121bae8c
Vincent Fumo <neodem@gmail.com>
2020-10-10 16:13:21 -0400

    remove crap

--- atc/services/Grid.swift
+++ atc/services/Grid.swift
@@ -33,32 +33,5 @@ class Grid {
         let percentage: Float = Float(scalar) / Float(scaleFrom)
         return percentage * Float(scaleTo)
     }
-    
-    static func slope(p1: CGPoint, p2: CGPoint) -> CGFloat {
-        return (p2.y - p1.y) / (p2.x - p1.x);
-    }
-    
-    static func pointOnLineWithY(p: CGPoint, slope: CGFloat, y: CGFloat) -> CGPoint {
-        let x = (y - p.y) / slope + p.x;
-        return CGPoint(x:x, y:y);
-    }
-    
-    static func pointOnLineWithX(p: CGPoint, slope: CGFloat, x: CGFloat) -> CGPoint {
-        let y = slope * ( x - p.x ) + p.y
-        return CGPoint(x:x, y:y);
-    }
-    
-    
-    
-    static func insidePolygon(polygon: [CGPoint], point: CGPoint) {
-        var angle: Double
-        var p1: CGPoint
-        var p2: CGPoint
-        var n = polygon.count
-        
-        for i in 0...n {
-            let p1h = polygon[i].
-        }
-        
-    }
+
 }
